cf4679c62fc2b454b3372ef95f01b1b30761dc12
lastpingu <54551308+lazypwny751@users.noreply.github.com>
2021-07-12 03:01:12 +0300

    regularbash scripts

---
+++ index.sh
@@ -0,0 +1,11 @@
+maintainer="LazyPwny&ByCh4n"
+reponame="regular bash scripts for modulesh"
+
+packages=(
+  "https://raw.githubusercontent.com/ByCh4n-Group/regular-modules/main"
+  "color-1.0.0.tar.gz_modulesh-color_1.0.0"
+  "os-utils-1.0.0.tar.gz_modulesh-os-utils_1.0.0"
+  "text-utils-1.0.0.tar.gz_modulesh_text-utils_1.0.0"
+  "net-utils-1.0.0.tar.gz_modulesh-net-utils_1.0.0"
+  "spinners-1.0.0.tar.gz_modulesh-spinners_1.0.0"
+)
